bf13a68bb5b77fac88b1fd4e407b49af93864eb9
Lee Robinson <lrobinson2011@gmail.com>
2022-02-12 16:01:12 -0600

    Update dependencies.

--- api/server.js
+++ api/server.js
@@ -1,90 +1,79 @@
-// Native
-const url = require('url')
-
-// Packages
-const {send} = require('micro')
-const request = require('request-promise-native')
-
-const client_id = process.env.CLIENT_ID
-const client_secret = process.env.CLIENT_SECRET
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
 if (!client_id || !client_secret) {
-  console.error('Please define the necessary env variables.')
-  process.exit(1)
+  console.error('Please define the necessary env variables.');
+  process.exit(1);
 }
 
-const tokens = new Map()
+const tokens = new Map();
+
+export default function handler(req, res) {
+  console.log({ client_id, client_secret });
+
+  const keyCount = Object.keys(req.query).length;
+  if (keyCount < 1 || !req.query.state) {
+    return res.status(502).json({
+      error: 'invalid_request',
+    });
+  }
+
+  if (req.query.code && req.query.state) {
+    const token = await requestToken(req.query.code, req.query.state);
+    tokens.set(String(req.query.state), Buffer.from(token));
+
+    return res.status(200).end('Done. You can close this window now!');
+  }
+
+  if (req.query.state) {
+    const ID = req.query.state;
+
+    if (tokens.has(ID)) {
+      res.status(200).json({
+        token: tokens.get(ID).toString(),
+      });
+
+      // Securily wipe token from RAM
+      tokens.get(ID).fill(0);
+      tokens.delete(ID);
+
+      return;
+    }
+
+    return res.status(403).json({
+      error: 'state_not_defined',
+    });
+  }
+
+  return res.status(502).json({
+    error: 'invalid_request',
+  });
+}
 
 const requestToken = async (code, state) => {
   if (!code || !state) {
-    return false
+    return false;
   }
-
-  let response
 
   try {
-    response = await request({
-      uri: 'https://github.com/login/oauth/access_token',
-      method: 'POST',
-      body: {
-        code,
-        state,
-        client_id,
-        client_secret
-      },
-      json: true
-    })
+    const response = await fetch(
+      'https://github.com/login/oauth/access_token',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          code,
+          state,
+          client_id,
+          client_secret,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    const data = await response.json();
+    return data.access_token;
   } catch (error) {
-    console.error(error)
-    return false
+    console.error(error);
+    return false;
   }
-
-  return response.access_token
-}
-
-module.exports = async function (req, res) {
-  const {query} = url.parse(req.url, true)
-  const keyCount = Object.keys(query).length
-
-  if (keyCount < 1 || !query.state) {
-    send(res, 502, {
-      error: 'invalid_request'
-    })
-
-    return
-  }
-
-  if (query.code && query.state) {
-    const token = await requestToken(query.code, query.state)
-    tokens.set(String(query.state), Buffer.from(token))
-
-    send(res, 200, 'Done. You can close this window now!')
-    return
-  }
-
-  if (query.state) {
-    const ID = query.state
-
-    if (tokens.has(ID)) {
-      send(res, 200, {
-        token: tokens.get(ID).toString()
-      })
-
-      // Securily wipe token from RAM
-      tokens.get(ID).fill(0)
-      tokens.delete(ID)
-
-      return
-    }
-
-    send(res, 403, {
-      error: 'state_not_defined'
-    })
-
-    return
-  }
-
-  send(res, 502, {
-    error: 'invalid_request'
-  })
-}
+};
